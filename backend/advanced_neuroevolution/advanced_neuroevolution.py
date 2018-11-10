bf4875e2004c1d3d80a7d4bbc1b89f75f992c035
Ahmed Aly <aaa2cn@virginia.edu>
2018-11-10 15:18:15 -0500

    version 0.1.3

--- backend/advanced_neuroevolution/advanced_neuroevolution.py
+++ backend/advanced_neuroevolution/advanced_neuroevolution.py
@@ -1,1 +1,7 @@
-"""main script"""
+"""main script
+Its main functions are:
+1) Init a pool object
+2) Evaluate the models
+3) Get/Set the model weights
+4) Get the new pool
+"""
---
+++ backend/advanced_neuroevolution/elite.py
@@ -0,0 +1,1 @@
+"""base class for elite"""

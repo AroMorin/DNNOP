e64dff9f4586fa374bfef6dd455d62a7fccb530f
AroMorin <AroMorin@users.noreply.github.com>
2018-11-14 15:09:12 -0500

    Added model base class

---
+++ backend/advanced_neuroevolution/model.py
@@ -0,0 +1,6 @@
+"""Base class for a pyTorch model. The reasoning for creating a separate class
+file instead of defining the class as part of the environment is that
+the same model can be re-used for different classes. Thus, this approach
+eliminates repitition of model definition. Especially, by defining a base here
+and then building models off that base should reduce code repition.
+"""

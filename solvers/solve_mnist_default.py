61f065b0e26e316c88eee9f845a40ec4cc1ae212
Ahmed Aly <aaa2cn@virginia.edu>
2018-11-27 21:00:23 -0500

    updated MNIST example

---
+++ solvers/solve_mnist_default.py
@@ -0,0 +1,89 @@
+"""This script attempts to solve the classification problem of the MNIST
+dataset. Comet ML is used to automatically upload and document the results.
+"""
+from __future__ import print_function
+from ../models/cnn_mnist import Net
+import argparse
+from comet_ml import Experiment
+import torch
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+experiment = Experiment(api_key = "5xNPTUDWzZVquzn8R9oEFkUaa",
+                        project_name="mnist_examples", workspace="aromorin")
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device) # Remove
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device) # Remove
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return 100. * correct / len(test_loader.dataset)
+
+def main():
+    # Assumes CUDA is available
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=2, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    args = parser.parse_args()
+
+    hyper_params = {"learning_rate": args.lr, "epochs":args.epochs,
+                    "batch_size":args.batch-size}
+    experiment.log_multiple_params(hyper_params)
+
+    device = torch.device("cuda")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True}
+
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    for epoch in range(1, args.epochs+1):
+        """Inefficient code because it sends the data to memory batch by batch.
+        It's better to send the entire set to GPU memory, then to start
+        operating on batches. This is because there's overhead in moving
+        things from/to device/main memory.
+        """
+        train(args, model, device, train_loader, optimizer, epoch)
+        test_acc = test(args, model, device, test_loader)
+
+    experiment.log_metric("Validation accuracy (%)", test_acc)
+
+
+
+if __name__ == '__main__':
+    main()

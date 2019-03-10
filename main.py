b50a7b2a158e579813f3be518d9bc6f24034a2f4
Ahmed Aly <aaa2cn@virginia.edu>
2019-03-10 00:28:49 -0500

    test commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """This is the main script of the library.
 Here the solver script is imported and run. This creates a unified interface
 to the library for the end-user.
+I'll probably remove this.
 """
 import solvers.solve_mnist_default

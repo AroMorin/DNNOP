27fe1f8fdd1a18fcb93cf7e67679ff097c6a1653
Ahmed Aly <aaa2cn@virginia.edu>
2019-04-14 22:21:32 -0400

    intro of memory to learner2

--- backend/algorithms/learner2_backend/memory.py
+++ backend/algorithms/learner2_backend/memory.py
@@ -3,9 +3,22 @@
 from .perturbation import Perturbation
 import torch
 
-class Probes(object):
+class Memory(object):
     def __init__(self, hp):
         self.vector = None
+        self.observations = []
+        self.inferences = []
+        self.scores = []
+        self.matrix = None
+
+    def update(self, observation, inference, score):
+        if observation not in self.observations:
+            self.observations.append(observation)
+            self.inferences.append(inference)
+            self.scores.append(score)
+
+    def evaluate_hypothesis(self, hypothesis):
+        pass
 
     def generate(self, vector, perturb):
         """Set the new probes based on the calculated anchors."""
